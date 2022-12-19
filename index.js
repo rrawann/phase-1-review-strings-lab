9acf458a6fabd508cfe2b4757787402f92761b8a
Rawan <rawnq.sb@gmail.com>
2022-12-19 22:38:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 5)}!`;
+
